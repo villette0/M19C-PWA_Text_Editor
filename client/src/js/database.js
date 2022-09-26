71d970ab56695064223c951a3bafc08ae4d7f145
violet978 <villette@live.com>
2022-09-26 12:22:00 -0700

    Added put request

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,18 +15,31 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // export const putDb = async (content) => console.error('putDb not implemented');
 
+export const putDb = async () => {
+  console.log('PUT from the database');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.put(id);
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
+
+
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const jateDB = await openDB('contact', 1);
+  const jateDB = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDB.transaction('contact', 'readonly');
+  const tx = jateDB.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
